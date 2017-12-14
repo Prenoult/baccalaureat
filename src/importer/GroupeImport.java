c4c6d5a1a143a8676586cc7592b03b515fdac204
Prenoult <prenoult@me.com>
2017-12-14 19:01:48 +0100

    Ajout du fichier GroupeImport

---
+++ src/importer/GroupeImport.java
@@ -0,0 +1,105 @@
+package importer;
+
+import database.DBManager;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+
+public class GroupeImport {
+    private static final Logger LOG = Logger.getLogger(GroupeImport.class.getName());
+
+    private String filename;
+
+    public GroupeImport(String filename) {
+        super();
+        this.filename = filename;
+    }
+
+    public CSVParser buildCVSParser() throws IOException {
+        CSVParser res = null;
+        Reader in;
+        in = new FileReader(filename);
+        CSVFormat csvf = CSVFormat.DEFAULT.withCommentMarker('#').withDelimiter(';');
+        res = new CSVParser(in, csvf);
+        return res;
+    }
+
+    private boolean add(String code, String groupe) {
+        boolean res = true;
+        String sql = "";
+        PreparedStatement ps = null;
+        sql = "INSERT into attrgroupes(code,groupe)"
+                + " VALUES(?,?)";
+        try {
+            ps = DBManager.CONNECTION.prepareStatement(sql);
+            ps.setString(1, code);
+            ps.setString(2, groupe);
+            LOG.info(ps.toString());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            LOG.warning(e.getMessage());
+            res = false;
+        }
+        return res;
+    }
+
+    public int updateDB(CSVParser parser) {
+        int res = 0;
+        DBManager.connect();
+        boolean b = false;
+        for (CSVRecord item : parser) {
+            if (!b) {
+                b = true;
+            } else {
+                String code = item.get(0).trim();
+                String groupe = item.get(1).trim();
+
+                System.out.println(code);
+                System.out.println(groupe);
+                if (add(code, groupe)) {
+                    res++;
+                }
+            }
+        }
+        DBManager.quit();
+        return res;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Logger getLog() {
+        return this.LOG;
+    }
+
+    //===========================================
+    public static void main(String[] args) {
+
+        GroupeImport el = new GroupeImport("data/attr-groupes.csv");
+
+        DBManager.URI = "jdbc:mysql://127.0.0.1:3306/baccalaureat";
+        DBManager.USER = "root";
+        DBManager.PASSWORD = "root";
+        CSVParser p = null;
+        try {
+            p = el.buildCVSParser();
+            el.updateDB(p);
+        } catch (IOException e) {
+            LOG.severe(e.getMessage());
+        }
+
+    }
+}
