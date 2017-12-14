fd486b3522c09aeda67aa2550a02fe2bebd386e5
Prenoult <prenoult@me.com>
2017-12-14 19:11:33 +0100

    Ajout du fichier RemplacerImport

---
+++ src/importer/RemplacerImport.java
@@ -0,0 +1,105 @@
+package importer;
+
+import database.DBManager;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+
+public class RemplacerImport {
+    private static final Logger LOG = Logger.getLogger(RemplacerImport.class.getName());
+
+    private String filename;
+
+    public RemplacerImport(String filename) {
+        super();
+        this.filename = filename;
+    }
+
+    public CSVParser buildCVSParser() throws IOException {
+        CSVParser res = null;
+        Reader in;
+        in = new FileReader(filename);
+        CSVFormat csvf = CSVFormat.DEFAULT.withCommentMarker('#').withDelimiter(';');
+        res = new CSVParser(in, csvf);
+        return res;
+    }
+
+    private boolean add(String g1, String g2) {
+        boolean res = true;
+        String sql = "";
+        PreparedStatement ps = null;
+        sql = "INSERT into assocremplacer(g1,g2)"
+                + " VALUES(?,?)";
+        try {
+            ps = DBManager.CONNECTION.prepareStatement(sql);
+            ps.setString(1, g1);
+            ps.setString(2, g2);
+            LOG.info(ps.toString());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            LOG.warning(e.getMessage());
+            res = false;
+        }
+        return res;
+    }
+
+    public int updateDB(CSVParser parser) {
+        int res = 0;
+        DBManager.connect();
+        boolean b = false;
+        for (CSVRecord item : parser) {
+            if (!b) {
+                b = true;
+            } else {
+                String g1 = item.get(0).trim();
+                String g2 = item.get(1).trim();
+
+                System.out.println(g1);
+                System.out.println(g2);
+                if (add(g1, g2)) {
+                    res++;
+                }
+            }
+        }
+        DBManager.quit();
+        return res;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Logger getLog() {
+        return this.LOG;
+    }
+
+    //===========================================
+    public static void main(String[] args) {
+
+        RemplacerImport el = new RemplacerImport("data/assoc-remplacer.csv");
+
+        DBManager.URI = "jdbc:mysql://127.0.0.1:3306/baccalaureat";
+        DBManager.USER = "root";
+        DBManager.PASSWORD = "root";
+        CSVParser p = null;
+        try {
+            p = el.buildCVSParser();
+            el.updateDB(p);
+        } catch (IOException e) {
+            LOG.severe(e.getMessage());
+        }
+
+    }
+}
